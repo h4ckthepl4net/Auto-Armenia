d088fb0efe9670641af49ab90712fc12c603b3a3
Kar <karo.sahatchyan@mail.ru>
2019-12-16 22:14:33 +0400

    Add favicon.ico, logo.png, header.css, main.css, header.html and index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" type="image/ico" href="/assets/images/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+    <title>Auto Armenia</title>
+</head>
+<body>
+    <div class="page_main_container">
+        <div class="header">
+            <?php
+                session_start();
+                include("html/header.html");
+            ?>
+        </div>
+    </div>
+</body>
+</html>
